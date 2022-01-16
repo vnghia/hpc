5fe0f135f1d403cfb6cd42dbe79c3236842cdb1b
Vo Van Nghia <vanvnghia@gmail.com>
2022-01-16 16:38:49 +0100

    reports: add helper scripts for reports

---
+++ reports/utils.py
@@ -0,0 +1,41 @@
+from fractions import Fraction
+
+import numpy as np
+
+
+def init_matrix(nrow: int, ncol: int, coff: float) -> np.ndarray:
+    m = np.empty((nrow, ncol))
+    for i in range(nrow):
+        for j in range(ncol):
+            m[i, j] = coff * (i + 1 + j + 1) / nrow / ncol
+    return m
+
+
+def pmatrix(a: np.ndarray, is_frac: bool = False) -> str:
+    if len(a.shape) != 2:
+        raise ValueError("pmatrix can only display two dimensions")
+    latex = r"\begin{pmatrix}" + "\n"
+    nrow, ncol = a.shape
+    for i in range(nrow):
+        for j in range(ncol):
+            fraction, integer = np.modf(a[i, j])
+            if fraction == 0:
+                latex += str(int(integer))
+            else:
+                if is_frac:
+                    frac = Fraction(str(a[i, j])).limit_denominator()
+                    latex += (
+                        r"\frac{"
+                        + str(frac.numerator)
+                        + "}{"
+                        + str(frac.denominator)
+                        + "}"
+                    )
+                else:
+                    latex += str(a[i, j]).replace(".", ",")
+            if j != ncol - 1:
+                latex += " & "
+        if i != nrow - 1:
+            latex += r"\\" + "\n"
+    latex += "\n" + r"\end{pmatrix}"
+    return latex
