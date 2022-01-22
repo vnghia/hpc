665e1b0c3159f36a710f7ed56019cde8976969bf
Võ Văn Nghĩa <vanvnghia@gmail.com>
2022-01-22 14:26:30 +0100

    reports(mpi): add google matrix

--- mpi/mpi.r
+++ mpi/mpi.r
@@ -1,6 +1,10 @@
 ## ---- init ----
 
+library(dplyr)
 library(reticulate)
+library(kableExtra)
+library(stringr)
+library(tidyr)
 
 source(here::here("reports", "utils.r"))
 source_python(here::here("reports", "utils.py"))
@@ -72,3 +76,122 @@ power_introduction_result <- read_binary_mpi_output(
 
 ## ---- power-introduction-matrix-output
 writeLines(paste(power_introduction_result[[1]], collapse = "\n"))
+
+## ---- google-matrix ----
+
+# define dangling_nodes/google_matrix_dense
+py_run_string(paste(nb_sources[[5]][c(2:11)], collapse = "\n"))
+# define matvec_sparse
+py_run_string(paste(nb_sources[[7]][c(2:9)], collapse = "\n"))
+# define init_H
+py_run_string(paste(str_replace_all(
+  nb_sources[[6]],
+  fixed("load_npz(\""), sprintf(
+    "load_npz(\"%s",
+    here::here("mpi", "")
+  )
+), collapse = "\n"))
+# define gdense source code
+gdense_init <- paste(nb_sources[[5]][15], init_n_u_v_from_h,
+  sep = "\n", collapse = "\n"
+)
+gdense_source <- paste(nb_sources[[5]][20:21], collapse = "\n")
+# define gsparse source code
+gsparse_init <- paste(nb_sources[[7]][15], init_n_u_v_from_h,
+  sep = "\n", collapse = "\n"
+)
+gsparse_source <- paste(nb_sources[[7]][17:18], collapse = "\n")
+# define gdense/gsparse mpi source path
+gdense_mpi_source <- here::here("mpi", "pagerank_dense.py")
+gsparse_mpi_source <- here::here("mpi", "pagerank_sparse.py")
+
+init_h_matrix_str <- function(p) {
+  return(paste0("H = init_H(", p, ")"))
+}
+
+init_matrix_params <- function(types) {
+  params <- tibble(type = types) %>%
+    mutate(
+      matrix = mapply(Matrix, as.integer(type)),
+      h_init = init_h_matrix_str(as.integer(type)),
+      shape = case_when(
+        type == 5 ~ 212711L,
+        T ~ as.integer(10^type)
+      )
+    ) %>%
+    select(-type)
+  return(params)
+}
+
+init_algo_params <- function(types, nps) {
+  params <- tibble(type = types, np = nps) %>%
+    mutate(
+      algo = mapply(Algo, as.integer(type)),
+      init = case_when(np != 0 ~ "", type ==
+        1 ~ gdense_init, type == 2 ~ gsparse_init),
+      s = case_when(
+        type == 1 & np == 0 ~ gdense_source,
+        type == 1 & np != 0 ~ gdense_mpi_source,
+        type == 2 & np == 0 ~ gsparse_source,
+        type == 2 & np != 0 ~ gsparse_mpi_source
+      )
+    ) %>%
+    select(-type)
+  return(params)
+}
+
+## ---- google-dense ----
+power_gdense_output_path <- here::here(
+  "mpi", "bin",
+  "output", "power_gdense.txt"
+)
+py_run_string(gdense_init)
+power_gdense_result <- read_binary_mpi_output(
+  power_gdense_output_path,
+  gdense_source
+)
+writeLines(paste(power_gdense_result[[1]], collapse = "\n"))
+
+## ---- google-sparse ----
+power_gsparse_output_path <- here::here(
+  "mpi", "bin",
+  "output", "power_gsparse.txt"
+)
+py_run_string(gsparse_init)
+power_gsparse_result <- read_binary_mpi_output(
+  power_gsparse_output_path,
+  gsparse_source
+)
+writeLines(paste(power_gsparse_result[[1]], collapse = "\n"))
+
+## ---- google-dense-sparse
+
+matrix_params <- init_matrix_params(c(1, 2, 3, 4, 5))
+algo_params <- init_algo_params(c(1, 2), rep(0L, 2))
+dense_sparse_params <- matrix_params %>%
+  crossing(algo_params) %>%
+  unite("init", c(h_init, init), sep = "\n")
+google_dense_sparse_db <- DBMPI(
+  matrices = dense_sparse_params$matrix,
+  shapes = dense_sparse_params$shape, algos = dense_sparse_params$algo,
+  nps = dense_sparse_params$np, algo_sources = dense_sparse_params$s,
+  inits = dense_sparse_params$init
+)
+google_dense_sparse_df <- google_dense_sparse_db$to_df(c(
+  "algo",
+  "time", "matrix", "shape", "important", "density",
+  "memory"
+))
+
+## ---- google-dense-sparse-output
+
+google_dense_sparse_df %>%
+  kbl(booktabs = T, escape = F, align = rep(
+    "r",
+    6
+  ), format.args = list(scientific = F), linesep = c(
+    "",
+    "\\addlinespace"
+  ), caption = "dense vs
+sparse approach") %>%
+  kable_styling(latex_options = c("hold_position"))
